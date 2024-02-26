497c9375786c897e37c56bc42ac11ea50703ff84
Max Schäfer <schaefer@medialis.one>
2024-02-26 11:23:36 +0100

    Revert "fix: log full issue instead of just .Fields during create"
    This reverts commit 6e9118fa9353fd0a54cdca477948fe69bbbad218.

--- pkg/notify/notify.go
+++ pkg/notify/notify.go
@@ -394,8 +394,7 @@ func (r *Receiver) reopen(issueKey string) (bool, error) {
 }
 
 func (r *Receiver) create(issue *jira.Issue) (bool, error) {
-	level.Debug(r.logger).Log("msg", "create", "issue", fmt.Sprintf("%+v", *issue))
-
+	level.Debug(r.logger).Log("msg", "create", "issue", fmt.Sprintf("%+v", *issue.Fields))
 	newIssue, resp, err := r.client.Create(issue)
 	if err != nil {
 		return handleJiraErrResponse("Issue.Create", resp, err, r.logger)
